e9086f8193cd6344ac8e17bfba3d393243603c2e
Bruno Penner <brunopenner01@gmail.com>
2024-07-25 13:20:03 +0800

    #76. The  class - Registering the table for use

--- post-types/class.mv-translations-cpt.php
+++ post-types/class.mv-translations-cpt.php
@@ -5,6 +5,8 @@ if (!class_exists('MV_Translations_Post_Type')) {
         public function __construct() {
             add_action('init', array($this, 'create_post_type'));
             add_action('init', array($this, 'create_taxonomy'));
+            add_action('init', array($this, 'register_metadata'));
+            add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
         }
 
         public function create_post_type() {
@@ -52,6 +54,28 @@ if (!class_exists('MV_Translations_Post_Type')) {
                     'show_admin_column' => true
                 )  
             );
+        }
+
+        public function register_metadata() {
+            global $wpdb;
+            $wpdb->translationmeta = $wpdb->prefix . 'translationmeta';
+        }
+
+        public function add_meta_boxes()
+        {
+            add_meta_box(
+                'mv_translations_meta_box',
+                esc_html__('Translations Options', 'mv-translations'),
+                array($this, 'add_inner_meta_boxes'),
+                'mv-translations',
+                'normal',
+                'high'
+            );
+        }
+
+        public function add_inner_meta_boxes($post)
+        {
+            require_once (MV_TRANSLATIONS_PATH . 'views/mv-translations_metabox.php');
         }
     }
 }
